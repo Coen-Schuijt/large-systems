5e5a4bd7868266a037273f3906615b9fbed54b6c
root <root@grenoble.studlab.os3.nl>
2018-12-19 10:51:37 +0100

    Finished log parser

--- parser/log_parser.py
+++ parser/log_parser.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import os
-import csv
 import sys
-import pandas as pd
 
 def get_args():
+    """Gets the arguments from the command line"""
     try:
         path = sys.argv[1]
         return path
@@ -13,31 +12,51 @@ def get_args():
         print("Wrong arguments. Usage:\n./parse.py dir\ndir = Relative path to csv files")
         sys.exit(1)
 
-def create_df(data):
-    df = pd.Dataframe(data)
+def return_seconds(item):
+    """
+    Function that splits the first part of the time mesurements, and returns the result in seconds.
+    Takes  : Time measurement in the format [mm:ss.ffffff]
+    Returns: Time measurement in the format   [sss.ffffff]
+    """
 
-def rmv_prep_zero(item):
-    if item.startswith('0'):
-        item_no_prep_zero = item[1:]
-    return item_no_prep_zero
+    # Parse minutes and seconds
+    minutes = item[0:2]
+    int_minutes = int(minutes)
+    seconds = item[3:5]
+    int_seconds = int(seconds)
+    
+    # Everything after the comma, to be concatenated later.
+    res_stripped = item[5:]
 
-def min_to_sec(item):
-    return
+    # If there are minutes, add them to seconds
+    if int(minutes) > 0:
+        int_seconds += int_minutes*60
+        res_in_seconds = str(int_seconds) + res_stripped
+        return res_in_seconds
+    # Otherwise, strip the zero
+    elif int(minutes) == 0:
+        res_in_seconds = str(int_seconds) + res_stripped
+        return res_in_seconds
 
 def parser(path):
-    
+    """
+    Reads all the log files in the provided path and parses the time measurement results accordingly.
+    Takes   : Relative pat to the log files
+    Returns : Several arrays with parsed results
+    """
+   
+    # Initialize empty arrays
     total_arr_10 = []
     docker_arr_10 = []
     orchestration_arr_10 = []
-    
     total_arr_100 = []
     docker_arr_100 = []
     orchestration_arr_100 = []
-
     total_arr_200 = []
     docker_arr_200 = []
     orchestration_arr_200 = []
 
+    # Search for log files and parse the results
     for entry in os.listdir(path):
         if entry.endswith('.log'):
             if '_10_' in entry:
@@ -46,89 +65,56 @@ def parser(path):
                     stripped_data = data[-3:]
                     for e,item in enumerate(stripped_data):
                         result_10 = item.replace('\n','')
-                        seconds_10 = result_10[-9:]
-
-                        res_10 = rmv_prep_zero(seconds_10)
-
+                        before_comma_10 = result_10[-12:]
+                        result_10 = return_seconds(before_comma_10)
                         if e == 0:
-                            total_arr_10.append(res_10)
+                            total_arr_10.append(result_10)
                         elif e == 1:
-                            docker_arr_10.append(res_10)
+                            docker_arr_10.append(result_10)
                         elif e == 2:
-                            orchestration_arr_10.append(res_10)
+                            orchestration_arr_10.append(result_10)
             elif '_100_' in entry:
                 with open(path + '/' + entry, 'r') as f_100:
                     data = f_100.readlines()
                     stripped_data = data[-3:]
                     for e,item in enumerate(stripped_data):
                         result_100 = item.replace('\n','')
-                        res_100 = result_100[-9:]
-       
-                        # Check if seconds starts with 0
-#                        seconds = res_200[2:4]
-
+                        before_comma_100 = result_100[-12:]
+                        result_100 = return_seconds(before_comma_100)
                         if e == 0:
-                            total_arr_100.append(res_100)
+                            total_arr_100.append(result_100)
                         elif e == 1:
-                            docker_arr_100.append(res_100)
+                            docker_arr_100.append(result_100)
                         elif e == 2:
-                            if item.startswith('0'):
-                                item = item[:]
-                                print(item)
-                            else:
-                                orchestration_arr_100.append(res_100)
-
+                             orchestration_arr_100.append(result_100)
             elif '_200_' in entry:
                 with open(path + '/' + entry, 'r') as f_200:
                     data = f_200.readlines()
                     stripped_data = data[-3:]
                     for e,item in enumerate(stripped_data):
                         result_200 = item.replace('\n','')
-                        res_200 = result_200[-11:]
-        
-                        # Extract minutes
-                        minutes = res_200[0]
-                        int_mins = int(minutes)
-                        #print("minutes", minutes)
-                        #print(int_mins)
-
-                        # Extract seconds
-                        seconds = res_200[2:4]
-                        int_secs = int(seconds)
-                        #print("seconds", seconds)
-                        #print(int_secs)
-
-                        # If there is at least one minute
-                        if int_mins > 0:
-                            # Multiply the amount of minutes by 60 and add to seconds
-                            int_secs += int_mins*60
-
-                        # Split after comma
-                        res_200_rmvd_min = res_200[4:]
-                        #print("res_200_rmvd_min ", res_200_rmvd_min) 
-                        res_200_float = str(int_secs) + res_200_rmvd_min
-
-                        #print(res_200_float)
-
+                        before_comma_200 = result_200[-12:]
+                        result_200 = return_seconds(before_comma_200)                        
                         if e == 0:
-                            total_arr_200.append(res_200_float)
+                            total_arr_200.append(result_200)
                         elif e == 1:
-                            docker_arr_200.append(res_200_float)
+                            docker_arr_200.append(result_200)
                         elif e == 2:
-                            orchestration_arr_200.append(res_200_float)
+                            orchestration_arr_200.append(result_200)
 
-    print("Totals 10:          ", total_arr_10)
-    print("Docker 10:          ", docker_arr_10)
-    print("Orchestration 10:   ", orchestration_arr_10,'\n')
-
-    print("Totals 100:         ", total_arr_100)
-    print("Docker 100:         ", docker_arr_100)
-    print("Orchestration 100:  ", orchestration_arr_100,'\n')
-
-    print("Totals 100:         ", total_arr_200)
-    print("Docker 100:         ", docker_arr_200)
-    print("Orchestration 100:  ", orchestration_arr_200)
+    return total_arr_10, docker_arr_10, orchestration_arr_10, total_arr_100, docker_arr_100, orchestration_arr_100, total_arr_200, docker_arr_200, orchestration_arr_200
 
 if __name__ == "__main__":
     path = get_args()
-    parser(path)
+    
+    total_arr_10, docker_arr_10, orchestration_arr_10, total_arr_100, docker_arr_100, orchestration_arr_100, total_arr_200, docker_arr_200, orchestration_arr_200 = parser(path)
+
+    print("Totals ---------- 10: ", total_arr_10)
+    print("Docker ---------- 10: ", docker_arr_10)
+    print("Orchestration --- 10: ", orchestration_arr_10)
+    print("Totals --------- 100: ", total_arr_100)
+    print("Docker --------- 100: ", docker_arr_100)
+    print("Orchestration -- 100: ", orchestration_arr_100)
+    print("Totals --------- 200: ", total_arr_200)
+    print("Docker --------- 200: ", docker_arr_200)
+    print("Orchestration -- 200: ", orchestration_arr_200)
